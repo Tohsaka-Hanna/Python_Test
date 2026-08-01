d86fb910eae66200422affb871df1c929441d2eb
Jigoku No Hanna <hannahnohannah@gmail.com>
2026-07-31 22:47:39 -0500

    Add files via upload

---
+++ Python_Test/0_Matematicas/1_Matematicas.py
@@ -0,0 +1,108 @@
+#================================================
+# Ana tiene 8 manzanas y compra 5 más. ¿Cuántas manzanas tiene ahora?
+#================================================
+
+A = 8 #Manzana
+C = 5 #Compradas
+X = (A + C)
+
+print("Ecuacion #1")
+print(f"A = {A}\nC = {C}\n")
+print("Ecuacion (A + C) = X")
+print(f"Resultado ({A} + {C}) = {X}\n")
+
+#================================================
+# Un tren recorre 120 kilómetros por la mañana y 85 kilómetros por la tarde. ¿Cuántos kilómetros recorrió en total?
+#================================================
+
+M = 120 #Mañana
+T = 85  #Tarde
+R = (M + T) #Recorrido
+
+print("Ecuacion #2")
+print(f"M = {M}\nT = {T}\n")
+print("Ecuacion (M + T) = R")
+print(f"Resultado ({M} + {T}) = {R}\n")
+
+#================================================
+# Carlos tenía 50 monedas y gastó 18 en una tienda. ¿Cuántas monedas le quedan?
+#================================================
+
+
+I = 50 #Inicial
+G = 18 #Gastos
+T = (I - G) #Total
+
+print("Ecuacion #3")
+print(f"I = {I}\nG = {G}\n")
+print("Ecuacion (I - G) = T")
+print(f"Resultado ({I} - {G}) = {T}\n")
+
+#================================================
+# En una caja hay 9 filas con 7 libros en cada una. ¿Cuántos libros hay en total?
+#================================================
+
+F = 9 # Caja
+L = 7 # Filas
+T = (F * L) #Total
+
+print("Ecuacion #4")
+print(f"F = {F}\nL = {L}\n")
+print("Ecuacion (F * L) = T")
+print(f"Resultado ({F} * {L}) = {T}\n")
+
+#================================================
+# Marta tenía 40 caramelos. Regaló 12 a un amigo y después compró 25 más. ¿Cuántos caramelos tiene ahora?
+#================================================
+
+C = 40 #Caramelos
+R = 12 #Regalo
+M = 25 #Mas caramelos
+T = (C - R) + M #Total
+
+print("Ecuacion #4")
+print(f"C = {C}\nR = {R}\nM = {M}\n")
+print("Ecuacion (C - R) = T")
+print(f"Resultado ({C} - {R}) = {T}")
+
+#================================================
+# Una tienda tiene 240 botellas de agua. Durante la mañana vende 75 y por la tarde vende el doble de lo que vendió 
+# por la mañana. ¿Cuántas botellas quedan al final del día?
+#================================================
+
+I = 240 #Inventario de botellas
+M = 75  #Ventas Manana
+T = 2*M #Ventas Tarde
+VT = (M + T) #Ventas totales
+IR = (I - VT) #Inventario restante
+
+print("Ecuacion #5")
+print(f"I = {I}\nM = {M}\nT = {T}\n")
+
+print("Ecuacion (2 * M) = T")
+print(f"Ecuacion (2 * {M}) = {T}\n")
+
+print("Ecuacion (M + T) = VT")
+print(f"Ecuacion ({M} + {T}) = {VT}\n")
+
+print("Ecuacion (I - VT) = IR")
+print(f"Ecuacion ({I} - {VT}) = {IR}\n")
+
+#================================================
+# Un agricultor plantó 18 filas de árboles y en cada fila hay 14 árboles. Después, decidió añadir 56 árboles más. 
+# ¿Cuántos árboles tiene ahora en total?
+#================================================
+
+F = 18 #filas
+C = 14 #columnas
+A = 56 #Arboles
+T = (F * C) + A #Total
+
+print("Ecuacion #6")
+print(f"F = {F}\nC = {C}\nA = {A}\n")
+print("Ecuacion (F * C) + A = T")
+print(f"Resultado ({F} * {C}) + {A} = {T}")
+
+#================================================
+#
+#================================================
